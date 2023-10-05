8e25020444d82e7410933658d2e71587588c18fe
Jaden-Crotty <145504748+Jaden-Crotty@users.noreply.github.com>
2023-10-05 17:44:08 -0400

    tested incorrect code given

---
+++ Coding set 3 .py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Algorithm workbench 
+
+# # number 4 
+
+# In[1]:
+
+
+# question 4 code shows an If-Else statement that is incorrect with inproper aligment we must fix the function 
+if score >= A_score: 
+print('Your grade is A.') 
+else: 
+if score >= B_score: 
+print('Your grade is B.') 
+else: 
+if score >= C_score: 
+print('Your grade is C.') 
+else: 
+if score >= D_score: 
+print('Your grade is D.') 
+else: 
+print('Your grade is F.') 
+
+
+# In[ ]:
+
+
+
+
